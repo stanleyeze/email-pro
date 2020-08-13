d10d27703f250083d571afd88e8ba6f86baa6b8f
kenstufsz <stanleyekene.ze@gmail.com>
2020-08-13 13:13:34 -0600

    backendd setup

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const app = express(morgan("tiny"));
+
+//body passer middleware
+app.use(express.json());
+//app.use(express.urlencoded()); form
+app.use(express.static("public"));
+app.use(helmet());
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan is enambled");
+}
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/api/list", (req, res) => {
+  console.log(req.body);
+
+  //perfore the soting here
+
+  //return sorted
+  res.send(req.body.email);
+});
+
+app.delete("/api/list/:id", (req, res) => {
+  //check if course exists...
+  const course = courses.find((c) => c.id === req.params.id);
+  if (!course)
+    return res.status(400).send("The course with given ID not found!");
+
+  const index = couses.indexOf(course);
+  courses.slice(index, 1);
+
+  //perfore the soting here
+
+  //return sorted
+  res.send(course);
+});
+
+app.listen(3000, () => {
+  console.log("listening at port 3000...");
+});
